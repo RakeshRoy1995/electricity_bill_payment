36f1a5b80943d5a53ac0b0f552d03cf53f219024
Rakesh Roy <rakeshroyshuvo@gmail.com>
2019-02-07 20:17:14 +0600

    Bill Payment

---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include("connect.php");
+if(!(isset($_SESSION['log_user'])))
+{
+	header("location:check.php");
+}
+else
+{	
+	$user_name=$_SESSION['log_user'];
+?>
+<html>
+<head>
+<meta http-equiv="refresh" content="10; url=schpayment.php">
+</head>
+<body>
+<center>
+<h1>Roy Electricity Bill Payment</h1>
+<hr>
+<p>
+Your Will be redirect to payment<br>
+Note: Do not refresh or close this page
+</p>
+Please wait...
+</center>
+</body>
+</html>
+<?php
+}
+?>
